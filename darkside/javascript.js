fa20610ba4c5a27dd3a994cd791116c5f69dca10
bpn333 <bipinlamsal2004@gmail.com>
2023-03-29 23:32:59 +0530

    lets see

--- darkside/javascript.js
+++ darkside/javascript.js
@@ -1,16 +1,11 @@
-document.getElementById("add").addEventListener("click", add);
-document.getElementById("sub").addEventListener("click", sub);
-document.getElementById("mul").addEventListener("click", mul);
-document.getElementById("div").addEventListener("click", div);
-function add(){
-	document.getElementById("java").innerHTML = parseInt(document.getElementById("num1").value) + parseInt(document.getElementById("num2").value);
-}
-function sub(){
-	document.getElementById("java").innerHTML = parseInt(document.getElementById("num1").value) - parseInt(document.getElementById("num2").value);
-}
-function mul(){
-	document.getElementById("java").innerHTML = parseInt(document.getElementById("num1").value) * parseInt(document.getElementById("num2").value);
-}
-function div(){
-	document.getElementById("java").innerHTML = parseInt(document.getElementById("num1").value) / parseInt(document.getElementById("num2").value);
-}
+const audio = new Audio("yamate-kudesai.mp3");
+document.getElementById("b1").addEventListener("click" , function(){
+audio.play();
+audio.addEventListener("play", function() {
+  b1.style.display = "none";
+});
+
+audio.addEventListener("pause", function() {
+  b1.style.display = "block";
+});
+});
